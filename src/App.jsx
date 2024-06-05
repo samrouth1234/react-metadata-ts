1d66a58fbd7951dfd157aa256a1119eeb55dae6d
samrouth <bichsamroth@gmail.com>
2024-06-05 16:12:57 -0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
-import { Helmet } from "react-helmet";
 
 import "./App.css";
 import AppRouter from "./AppRouter";
 
 const App = () => {
 
-  const metadata = {
-    title: "Home - Document Designer",
-    meta: {
-      description:"Likhet is a document report generator without the hassle of writing documents from scratch. ",
-      keywords:"Instant Template Upload, prefill field and history log, Create documents in DOCX or PDF instantly.",
-    },
-    openGraph: {
-      title: "Document Designer",
-      description:
-        "Likhet is a document report generator without the hassle of writing documents from scratch.",
-      url: "https://developers-cambodia.com/",
-      siteName: "Document Designer",
-      images: [
-        {
-          url: "/public/assets/SEO/DC.png",
-          width: 600,
-          height: 315,
-        },
-      ],
-      creator: "Digital Government Committee Team",
-    },
-    canonical: "https://developers-cambodia.com/",
-    structuredData: {
-      type: "WebPage",
-      url: "https://developers-cambodia.com/",
-      name: "Document Designer",
-      description: "A new evolution of IT community support in Khmer language.",
-    },
-  };
-
   return (
     <>
-      <Helmet>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.meta.description} />
-        <meta name="keywords" content={metadata.meta.keywords} />
-        {/* Open Graph meta tags */}
-        <meta property="og:title" content={metadata.openGraph.title} />
-        <meta
-          property="og:description"
-          content={metadata.openGraph.description}
-        />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:site_name" content={metadata.openGraph.siteName} />
-        <meta property="og:image" content={metadata.openGraph.images[0].url} />
-        <meta
-          property="og:image:width"
-          content={metadata.openGraph.images[0].width}
-        />
-        <meta
-          property="og:image:height"
-          content={metadata.openGraph.images[0].height}
-        />
-        <meta property="og:creator" content={metadata.openGraph.creator} />
-        <link rel="canonical" href={metadata.canonical} />
-        <script type="application/ld+json">
-          {JSON.stringify({
-            "@context": "http://schema.org",
-            "@type": metadata.structuredData.type,
-            url: metadata.structuredData.url,
-            name: metadata.structuredData.name,
-            description: metadata.structuredData.description,
-          })}
-        </script>
-      </Helmet>
-      {/* App Router */}
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,16 +1,59 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Footer from '../components/footer/Footer'
-import NavBar from '../components/navbar/NavBar'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/footer/Footer";
+import NavBar from "../components/navbar/NavBar";
+import { Helmet } from "react-helmet";
+
+export const metadata = {
+  title: "Home page",
+  description: "This is Project Demo Page",
+  metadataBase: new URL("https://acme.com"),
+  locale: "en-US",
+  mainfest: "/mainfest.json",
+  type: "wen site",
+  siteName: "Prodict",
+  category: "education",
+  openGraph: {
+    title: "Open Graph",
+    description: "Interactive",
+    url: "/",
+    siteName: "Open Graph",
+    images:
+      "https://online.hbs.edu/Style%20Library/api/resize.aspx?imgpath=/PublishingImages/overhead-view-of-business-strategy-meeting.jpg&w=1200&h=630",
+  },
+};
 
 export default function AppLayout() {
   return (
     <div>
-      <NavBar/>
+      <Helmet>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="locale" content={metadata.locale} />
+        <link rel="manifest" href={metadata.manifest} />
+
+        {/* Open Graph metadata */}
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+        <meta property="og:image" content={metadata.openGraph.images} />
+
+        {/* Additional metadata if needed */}
+        <meta name="category" content={metadata.category} />
+        <meta name="type" content={metadata.type} />
+        <meta name="siteName" content={metadata.siteName} />
+      </Helmet>
+      <NavBar />
       <div>
-        <Outlet/>
+        <Outlet />
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
